b9f833f37d5aeddc146adf4f83fc7f0cfb2bb8f7
Rasheen Kirkland <kirkland.rasheen@gmail.com>
2024-07-02 13:02:12 -0700

    completed assignment

--- backend/config/config.js
+++ backend/config/config.js
@@ -9,17 +9,17 @@ module.exports = {
     "dialect": "postgres"
   },
   "test": {
-    "username": "root",
+    "username": "postgres",
     "password": null,
     "database": "database_test",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "postgres"
   },
   "production": {
-    "username": "root",
+    "username": "postgres",
     "password": null,
     "database": "database_production",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "postgres"
   }
 }
